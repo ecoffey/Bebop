c3c985cb41dc38d52c6c7b072e4dadd0370223e4
Eoin Coffey <ecoffey@gmail.com>
2009-07-31 23:48:48 -0600

    Moved Route Factory to its own file

--- Bebop.Core/BebopRoute.cs
+++ Bebop.Core/BebopRoute.cs
@@ -9,7 +9,7 @@ namespace Bebop
 {
 	public abstract class BebopRoute : Route
 	{
-		public BebopRoute(string url, Type viewType, IContainer container)
+		internal BebopRoute(string url, Type viewType, IContainer container)
 			: base(url, new RouteHandler(viewType, container))
 		{
 		}
@@ -18,30 +18,10 @@ namespace Bebop
 
 	public sealed class BebopRoute<TView> : BebopRoute where TView : IView
 	{
-		public BebopRoute(string url, IContainer container)
+		internal BebopRoute(string url, IContainer container)
 			: base(url, typeof(TView), container)
 		{
 		}
 
-	}
-
-	public sealed class BebopRouteFactory
-	{
-		private IContainer _container;
-
-		internal BebopRouteFactory(IContainer container)
-		{
-			if (container == null)
-			{
-				throw new ArgumentNullException("container");
-			}
-
-			_container = container;
-		}
-
-		public BebopRoute<TView> Create<TView>(string url) where TView : IView
-		{
-			return new BebopRoute<TView>(url, _container);
-		}
 	}
 }
---
+++ Bebop.Core/BebopRouteFactory.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Autofac;
+
+namespace Bebop
+{
+	public sealed class BebopRouteFactory
+	{
+		private IContainer _container;
+
+		internal BebopRouteFactory(IContainer container)
+		{
+			if (container == null)
+			{
+				throw new ArgumentNullException("container");
+			}
+
+			_container = container;
+		}
+
+		public BebopRoute<TView> Create<TView>(string url) where TView : IView
+		{
+			return new BebopRoute<TView>(url, _container);
+		}
+	}
+}
