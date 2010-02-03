de8eb752bc07834942f295322b277d48cc43eb31
Eoin Coffey <ecoffey@gmail.com>
2010-02-03 16:26:14 -0700

    missing files

---
+++ Bebop.Core/IResource.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+
+namespace Bebop
+{
+	public interface IResource
+	{
+		IResourceResponse Get(ResourceRequestContext context);
+
+		IResourceResponse Post(ResourceRequestContext context);
+
+		IResourceResponse Put(ResourceRequestContext context);
+
+		IResourceResponse Delete(ResourceRequestContext context);
+	}
+}
---
+++ Bebop.Core/Resource.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Bebop
+{
+    public abstract class Resource : IResource
+    {
+		public virtual IResourceResponse Get(ResourceRequestContext context)
+		{
+			throw new InvalidOperationException("This view does not support the verb 'GET'");
+		}
+
+		public virtual IResourceResponse Post(ResourceRequestContext context)
+        {
+            throw new InvalidOperationException("This view does not support the verb 'POST'");
+        }
+
+		public virtual IResourceResponse Put(ResourceRequestContext context)
+		{
+			throw new InvalidOperationException("This view does not support the verb 'PUT'");
+		}
+
+		public virtual IResourceResponse Delete(ResourceRequestContext context)
+		{
+			throw new InvalidOperationException("This view does not support the verb 'DELETE'");
+		}
+    }
+}
