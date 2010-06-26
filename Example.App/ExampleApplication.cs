6299d2908313f0ca52d80f81a1c4a5b3ad258ad8
Eoin Coffey <ecoffey@gmail.com>
2010-06-26 10:37:58 -0600

    Updated to VS2010 / .Net 4.  New Example project and app

---
+++ Example.App/ExampleApplication.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Bebop;
+
+namespace Example.App
+{
+	public class Index : Resource
+	{
+		public override IResourceResponse Get(ResourceRequestContext context)
+		{
+			return new SimpleResponse("blah");
+		}
+	}
+
+	public class ExampleApplication : BebopApplication
+	{
+		public override IEnumerable<BebopRoute> Map(BebopRouteFactory routeFactory)
+		{
+			yield return routeFactory.Create<Index>("");
+		}
+	}
+}
---
+++ Example.Project/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Example.Project.Global" Language="C#" %>
